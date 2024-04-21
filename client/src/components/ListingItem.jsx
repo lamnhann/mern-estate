d36736cb3495af3c35765211c12a90b361b38a5b
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2024-04-21 15:21:05 +0700

    complete home page

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { MdLocationOn } from 'react-icons/md'
 
 export default function ListingItem({listing}) {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[270px]">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={listing.imageUrls[0] || "https://www.brittany.com.ph/wp-content/uploads/2021/06/House-and-lot-for-sale-investment-in-Sta-Rosa-House-and-lot-Brittany-Sta.-Rosa.png"}
